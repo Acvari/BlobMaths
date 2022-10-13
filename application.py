17302d9416b215c9014a5319e07c678babd24367
jamesm21 <jamesm21@uni.coventry.ac.uk>
2022-10-13 23:55:16 +0100

    Added login forms using flask_wtf (WTFforms' Flask extension) and flask-bootstrap

--- application.py
+++ application.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect
+from config import Config
+from forms import LoginForm
+from flask_bootstrap import Bootstrap
 
 application = Flask(__name__)
 app = application
-
+Bootstrap(app)
+app.config.from_object(Config)
 @app.route('/')
 def home():
 	return 'Hi'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash(f'Login requested for user {form.username.data}.')
+		return redirect('/appwelcome')
+	return render_template('login.html', title='Login', form=form)
+@app.route('/appwelcome')
+def app_welcome():
+	return "You have logged in successfully!"
