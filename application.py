bfd6d442125a662b4b903c3470f01ae26148b68a
Adam <sellwooda@coventry.ac.uk>
2022-10-14 22:08:57 +0100

    set up basic page - need to add questions

--- application.py
+++ application.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 application = Flask(__name__)
 app = application
+Bootstrap(app)
 
 @app.route('/')
 def home():
 	return 'Hi there'
+
+@app.route('/game')
+def game():
+	return render_template('base.html', title='Game')
