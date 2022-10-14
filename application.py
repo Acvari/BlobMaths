ba31cf98c214294b5a3327fc3ee03755f6d22e99
Adam <sellwooda@coventry.ac.uk>
2022-10-15 00:36:43 +0100

    added simple question and answer

--- application.py
+++ application.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
+from questionForm import answerForm
 
 application = Flask(__name__)
 app = application
 Bootstrap(app)
+app.config.from_pyfile("config.py")
 
 @app.route('/')
 def home():
 	return 'Hi there'
 
-@app.route('/game')
+@app.route('/game', methods=['GET', 'POST'])
 def game():
-	return render_template('base.html', title='Game')
+	answerform = answerForm()
+	if answerform.validate_on_submit():
+		if answerform.answer.data==answerform.answers:
+			flash("Correct!")
+		
+	return render_template('game.html', title='Game', form=answerform)
