928275e9d4a8b60be971db89cc9751bd96ab2b5d
Adam <sellwooda@coventry.ac.uk>
2022-10-15 13:31:24 +0100

    added wrong message

--- application.py
+++ application.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from questionForm import answerForm
 
+#initialise application
 application = Flask(__name__)
 app = application
+
 Bootstrap(app)
 app.config.from_pyfile("config.py")
 
@@ -17,5 +19,7 @@ def game():
 	if answerform.validate_on_submit():
 		if answerform.answer.data==answerform.answers:
 			flash("Correct!")
+		else:
+			flash("Not Quite!")
 		
 	return render_template('game.html', title='Game', form=answerform)
--- questionForm.py
+++ questionForm.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, StringField
+from wtforms import RadioField, FieldList
 from wtforms.validators import DataRequired
 
 class answerForm(FlaskForm):
