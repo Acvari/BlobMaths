4b5be84016cb7d5159f2e0c5dc01f72d36ba3786
duprej <duprej@coventry.ac.uk>
2022-10-18 09:41:31 +0100

    Created module selection page. From login page to selection page to game page.

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager, current_user, login_user
 from user_placeholder import User
 from questionForm import answerForm
 
-#initialise application
+# initialise application
 application = Flask(__name__)
 app = application
 login = LoginManager(app)
@@ -16,52 +16,66 @@ login.init_app(app)
 app.config.from_object(Config)
 user = User()
 
+
 @login.user_loader
 def load_user(id):
-	return user
+    return user
+
 
 @app.route('/')
 def home():
-	return redirect('/login')
+    return redirect('/login')
+
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-	print(current_user.nickname)
-	answerform = answerForm()
-	flash(f"Hello " + user.nickname)
-	if answerform.validate_on_submit():
-		if answerform.answer.data==answerform.answers:
-			flash("Correct!")
-		else:
-			flash("Not Quite!")
+    print(current_user.nickname)
+    answerform = answerForm()
+    flash(f"Hello " + user.nickname)
+    if answerform.validate_on_submit():
+        if answerform.answer.data == answerform.answers:
+            flash("Correct!")
+        else:
+            flash("Not Quite!")
 
-	return render_template('game.html', title='Game', form=answerform)
+    return render_template('game.html', title='Game', form=answerform)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	print(current_user)
-	if current_user.is_authenticated:
-		return redirect('/game')
-	form = LoginForm()
-	if form.validate_on_submit():
-		if (form.username.data != 'Batting Chestum') and (form.password.data != 'bigload420'):
-			flash(f'Incorrect login requested for user {form.username.data}.')
-			return redirect('/login')
-		login_user(user)
-		return redirect('/game')
-	return render_template('login.html', title='Login', form=form)
+    print(current_user)
+    if current_user.is_authenticated:
+        return redirect('/game')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (form.username.data != 'a') and (form.password.data != 'a'):
+            flash(f'Incorrect login requested for user {form.username.data}.')
+            return redirect('/login')
+        login_user(user)
+        return redirect('/moduleSelection')
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
-	print(user.nickname)
-	print(user.photo)
-	form = ProfileForm()
-	if form.validate_on_submit():
-		user.nickname = form.nickname.data
-		user.photo = form.photo.data
-		if form.nickname.data == '':
-			flash('Please choose a nickname!')
-			return redirect('/profile')
-		else: 
-			return redirect('/game')
-	return render_template('profile.html', title = 'Profile', form=form)
+    print(user.nickname)
+    print(user.photo)
+    form = ProfileForm()
+    if form.validate_on_submit():
+        user.nickname = form.nickname.data
+        user.photo = form.photo.data
+        if form.nickname.data == '':
+            flash('Please choose a nickname!')
+            return redirect('/profile')
+        else:
+            return redirect('/game')
+    return render_template('profile.html', title='Profile', form=form)
+
+
+@app.route('/moduleSelection', methods=['GET', 'POST'])
+def module_selection():
+    return render_template('moduleSelection.html', title='Module Selection')
+
+
+if __name__ == "__main__":
+    app.run()
